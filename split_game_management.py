37aedb28b58511dfda7f118b19bf74bc994240c1
AlanPrignon <35802519+scarfaceuuh@users.noreply.github.com>
2018-03-05 16:11:08 +0100

    Add files via upload

---
+++ split_game_management.py
@@ -0,0 +1,23 @@
+def ui_informations():
+    """
+    Display, on the screen, the UI informations for the player
+    
+    Version:
+    specification : Alan Prignon (v1 05/03/18) 
+    """
+
+def portals_informations():
+    """
+    Display, on the screen , the portals informations for the player
+    
+    Version:
+    specification : Alan Prignon (v1 05/03/18)
+    """
+
+def asteroids_informations():
+    """
+    Display, on the screen , the asteroids informations for the player
+    
+    Version : 
+    specification : Alan Prignon (v1 05/03/18)
+    """
