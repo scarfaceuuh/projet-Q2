454aff8165317355967d21e0714ded5cf566756e
BrunoBeyers <36985068+BrunoBeyers@users.noreply.github.com>
2018-03-05 15:18:48 +0100

    Add files via upload

---
+++ Projet.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+#PRÉ-PHASE **********************
+def game_management():
+    """
+        Regroupe les informations de l'ui, de portails et des astéroides.
+
+        version:
+        -------
+        specification: Bechet Francois (v.1 02/03/18)
+    """
+
+def create_ui (nbr_line,nbr_column):
+    """
+        Création de l'interface graphique.
+
+        Paramètres:
+        -----------
+        nbr_line: nombre de lignes en y (int)
+        nbr_column: nombre de colonnes en x (int)
+
+        Version:
+        --------
+        specification: Beyers Bruno (v1.1 03/03/18)
+
+    """
+def create_portals (pos_1_line,pos_1_column,pos_2_line,pos_2_column):
+    """
+        Mise en place et attribution des points de vie des portals
+
+        Paramètres:
+        -----------
+        pos_1_line: position en x du premier portail
+        pos_1_column: position en y du premier portail
+        pos_2_line: position en x du second portail
+        pos_2_column: position en y du second portail
+
+        Version:
+        --------
+        specification: Beyers Bruno (v1.0 03/03/18)
+
+
+    """
+def create_asteroids():
+    """
+        ##Comment savoir combien il y a d'astéroides dans le fichier? Crer fct get_number_astéroide et puis?
+
+    """
+#PHASE 1 **************************
+def phase_one():
+    """
+        regrouper les informations sur les vaisseaux
+
+        Version:
+        --------
+        specification: Beyers Bruno (v1.0 03/02/18)
+    """
+
+def store(ores):
+    """
+        Achat de vaisseaux
+
+        parameter:
+        ----------
+        ores: nombre de minerais (int)
+
+        Note:
+        -----
+        Le nombre de minerais doit être strictement positif
+
+        version:
+        --------
+        specification: Beyers Bruno (v.1 03/03/18)
+
+    """
+def vaisseau ()
+    """
+        rechercher les caractéristiques des vaisseaux dans un fichier
+    :return: 
+    """
+def attack(damage, scope):
+    """ attaque l'adversaire si la porté est suffisante
+
+        parameters:
+        -----------
+        damage: Points de dégat infliger (int)
+        scope: portée de l'attaque (int)
+
+        Note:
+        ----
+        La portée et les dégats doivent être strictement positif
+
+        version:
+        --------
+        specification: Beyers Bruno (v.1 02/03/18)
+    """
+
+#PHASE 2 **********************************
+def lock_unlock_asteroid(pos_x_asteroid,pos_y_asteroid):
+    """
+        Verrouiller et déverouille un astéroide, pour la récole automatique
+
+        Paramètres:
+        -----------
+        pos_x_asteroid: position en x d'un astéroide
+        pos_y_asteroid: poistion y d'un astéroide
+
+        Note:
+        -----
+        La postion verouillée doit contenir un astéroide
+        Ne peut être lock que par un extracteur
+
+        Version:
+        --------
+        specification: Beyers Bruno (v1.0 04/03/18)
+    """
+def lock_unlock_portail(pos_1_column,pos_1_line):
+    """
+        pos_1_line: position en x du premier portail
+        pos_1_column: position en y du premier portail
+
+        Note:
+        -----
+        Ne peut être lock que par un extracteur
+
+        Version:
+        --------
+        specification: Beyers Bruno (v1.0 05/03/18)
+    """
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+    # def health(health):
+    """ Points de vie des portails et vaisseaux
+
+        Parameter:
+        ----------
+        healt: Point de vie (int)
+
+        Note:
+        -----
+        Le vie doit être strictement positive 
+
+        version:
+        --------
+        specifcation: Beyers Bruno (v.1 02/3/18)
+    """
+
